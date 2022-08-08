69af535605f550b3f24fff802081b23640b441cd
Rob Siefert <robjsief@gmail.com>
2022-08-08 10:16:10 -0400

    fixed sql file for real

--- java/database/citytourDB.sql
+++ java/database/citytourDB.sql
@@ -13,6 +13,8 @@ CREATE TABLE address (
 	city_name varchar(50) NOT NULL,
 	state_name varchar(50) NOT NULL,
 	zip_code varchar(50) NOT NULL,
+	latitude float,
+	longitude float,
 	CONSTRAINT pk_address PRIMARY KEY (address_id)
 );
 
@@ -46,18 +48,18 @@ CREATE TABLE landmark_itinerary (
     CONSTRAINT fk_landmark_itinerary_landmark FOREIGN KEY (landmark_id) references landmark(landmark_id)
 );
 
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (4545, 'Rockefeller Plaza', 'New York', 'NY', 10111, 40.758730, -73.978700);
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (1000, '5th Ave', 'New York', 'NY', 10028, 40.779170, -73.962590 );
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (0, '','New York', 'NY', 10004, 40.6892, -74.044502);
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (558, 'Broadway', 'New York', 'NY', 10012, 40.7239, -73.9979);
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (30,'Rockefeller Plaza',  'New York', 'NY', 10112, 40.7593, -73.9794);
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (200,'Central Park West','New York', 'NY', 10024, 40.7813, -73.9740);
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (89,'E 42nd st','New York', 'NY', 10017, 40.7527, -73.9772);
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (20, 'Hudson Yards','New York', 'NY', 10001, 40.7538, -74.0022);
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (185, 'Greenwich St', 'New York', 'NY', 10006, 40.711399, -74.011146);
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (405, 'Lexington Ave', 'New York', 'NY', 10174, 40.7516, -73.9755 );
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (175, '5th Ave', 'New York', 'NY', 10010, 40.7411, -73.9897);
-INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitute) VALUES (1071, '5th Ave', 'New York', 'NY', 10128, 40.7830, -73.9590);
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (4545, 'Rockefeller Plaza', 'New York', 'NY', 10111, 40.758730, -73.978700);
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (1000, '5th Ave', 'New York', 'NY', 10028, 40.779170, -73.962590 );
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (0, '','New York', 'NY', 10004, 40.6892, -74.044502);
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (558, 'Broadway', 'New York', 'NY', 10012, 40.7239, -73.9979);
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (30,'Rockefeller Plaza',  'New York', 'NY', 10112, 40.7593, -73.9794);
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (200,'Central Park West','New York', 'NY', 10024, 40.7813, -73.9740);
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (89,'E 42nd st','New York', 'NY', 10017, 40.7527, -73.9772);
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (20, 'Hudson Yards','New York', 'NY', 10001, 40.7538, -74.0022);
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (185, 'Greenwich St', 'New York', 'NY', 10006, 40.711399, -74.011146);
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (405, 'Lexington Ave', 'New York', 'NY', 10174, 40.7516, -73.9755 );
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (175, '5th Ave', 'New York', 'NY', 10010, 40.7411, -73.9897);
+INSERT INTO address(street_number, street_name, city_name, state_name, zip_code, latitude, longitude) VALUES (1071, '5th Ave', 'New York', 'NY', 10128, 40.7830, -73.9590);
 
 
 INSERT INTO landmark (address_id, landmark_name, landmark_description) VALUES (1,'Rockefeller Center', 'Famous complex that is home to TV studios, plus a seasonal ice rink & giant Christmas tree.');
