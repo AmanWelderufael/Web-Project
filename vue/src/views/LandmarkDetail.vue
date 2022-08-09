d8c7f426a716bfc242893d4cddfb85bb5a8994e4
Rob Siefert <robjsief@gmail.com>
2022-08-09 11:20:03 -0400

    basic details page layout

--- vue/src/views/LandmarkDetail.vue
+++ vue/src/views/LandmarkDetail.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <p>{{landmark.landmark_name}}</p>
+  <div id ="gridContainer">
+    
+    <p id = "name">{{landmark.landmark_name}}</p>
+    <p id = "description">{{landmark.landmark_description}}</p>
+    <img id="landmark-picture" :src="getImageURL(landmark.landmark_id)" />
+    <img id="map" src='../assets/map.jpg'/>
+
   </div>
 </template>
 
@@ -19,7 +24,14 @@ export default {
       LandmarkService.getByID(this.$route.params.id).then(response =>{
         this.landmark = response.data;
       })
-    }
+    },
+    methods: {
+    getImageURL(id) {
+      console.log("../assets/" + id + ".jpg");
+      return require("../assets/" + id + ".jpg");
+    },
+  },
+    
   
 
 
@@ -27,5 +39,29 @@ export default {
 </script>
 
 <style>
+#name{
+  grid-area: name;
+}
+#description{
+  grid-area: description;
+}
+#landmark-picture{
+  grid-area: picture;
+  width: 100%;
+  height: 100%;
+
+}
+#map{
+  grid-area: map;
+  width: 100%;
+}
+
+#gridContainer{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-areas: "picture name map"
+                        "picture description map"
+                        "picture description map";
+}
 
 </style>
