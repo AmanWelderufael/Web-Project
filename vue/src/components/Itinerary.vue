af52b59aca30e9c43892ac6f8045e0bcf37a349a
Aman Welderufael <Amanwelderufael@gmail.com>
2022-08-16 13:18:29 -0400

    pull

--- vue/src/components/Itinerary.vue
+++ vue/src/components/Itinerary.vue
@@ -1,21 +1,27 @@
 <template>
-  
-    <div class="card">
-       <button @click="deleteItinerary()">Delete this trip</button>
-      <h2>{{ itinerary.itinerary_name }}</h2>
-      
-      <div v-for="landmark in landmarks" v-bind:key="landmark.landmark_id">
-        <h3>{{ landmark.landmark_name }}</h3>
-        <button @click="removeLandmark(landmark.landmark_id)">X</button>
-      </div>
-      
+  <div class="card">
+    <button class="deletebtn" @click="deleteItinerary()">
+      Delete this trip
+    </button>
 
-     
-      <div class="mapcard">
-        <itinerary-map v-bind:itinerary="this.itinerary" />
-      </div>
+    <h2>{{ itinerary.itinerary_name }}</h2>
+    <div v-for="landmark in landmarks" v-bind:key="landmark.landmark_id">
+      <p>{{ landmark.landmark_name }}</p>
+      <button class="xbtn" @click="removeLandmark(landmark.landmark_id)">X</button>
     </div>
+    
+<!-- <ul>
+<li v-for="landmark in landmarks" v-bind:key="landmark.landmark_id">
+  <p>{{ landmark.landmark_name }}</p>
+  <button class="delete" @click="removeLandmark(landmark.landmark_id)">X</button>
+</li>
+      </ul> -->
+    
 
+    <div class="mapcard">
+      <itinerary-map v-bind:itinerary="this.itinerary" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -57,7 +63,6 @@ export default {
 
 <style scoped>
 .card {
- 
   background-color: rgba(73, 41, 41, 0.7);
   border-radius: 18px;
   box-shadow: 2px 4px 12px rgb(0 0 0 / 8%);
@@ -65,21 +70,33 @@ export default {
 
   width: 100%;
   position: relative;
-  
 }
-button {
+li{
+   display: block;
+      padding: 0;
+      border: 1px solid #ddd;
+      margin-bottom: 5px;
+      background-color: #fff;
+}
+.delete{
+  
+  background: none;
+  border: 0px;
+  color: #888;
+  font-size: 15px;
+  width: 60px;
+  margin: 0px 0 0;
+  font-family: Lato, sans-serif;
+  cursor: pointer;
   float: right;
- 
-  
+
 }
-h3{
-  float: left;
-  
-}
+
 
 /* .card:hover {
   transform: rotate(3deg);
   cursor: pointer;
   transition-duration: 0.5s;
 } */
+
 </style>
